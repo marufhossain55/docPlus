0782bb2445602122cf3eb7f49ba25ee0def5721e
marufhossain55 <engrmaruf3@gmail.com>
2024-07-04 10:19:56 +0600

    gredient changed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,18 @@
 import banner from '../assets/banner.png';
-import './Banner.css';
-
 const Banner = () => {
   return (
-    <div>
-      <div className="banner-container mt-10">
-        <img src={banner} alt="banner" />
-      </div>
+    <div className="relative mt-10">
+      <img src={banner} alt="banner" className="w-full h-auto rounded-lg" />
+      <div
+        className="absolute inset-0 bg-gradient-to-r from-purple-900 to-transparent opacity-50 "
+        style={{
+          background:
+            'linear-gradient(90deg, rgba(2, 0, 67, 1), rgba(2, 0, 67, 0.3) 100%)',
+          borderRadius: '50px',
+        }}
+      ></div>
     </div>
   );
 };
+
 export default Banner;
